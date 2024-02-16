fd3665df77a74237c33bbbca1951c2d7fb6f15f2
vickypandey14 <vickypandeyts@gmail.com>
2024-02-16 23:38:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+// connecting mongo DB
+mongoose.connect("mongodb://127.0.0.1:27017/user-roles-permission");
+
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+const port = process.env.SERVER_PORT | 3000;
+
+app.listen(port, ()=>{
+    console.log("Server is running on port:- "+port);
+})
