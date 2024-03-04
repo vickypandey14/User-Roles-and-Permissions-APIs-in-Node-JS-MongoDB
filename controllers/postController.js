1af3eba3d2d0f3b59bc88a60d849bddd18eaeeb4
vickypandey14 <vickypandeyts@gmail.com>
2024-03-04 22:44:01 +0530

    Update Post API Method done

--- controllers/postController.js
+++ controllers/postController.js
@@ -132,19 +132,55 @@ const deletePost = async (req, res) => {
 // Update Post Details API Method
 
 const updatePost = async (req, res) => {
-
+    
     try {
+        const errors = validationResult(req);
 
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                success: false,
+                msg: 'Validation Error',
+                errors: errors.array()
+            });
+        }
 
-        
-    } catch (error) 
-    {
+        const { id, title, description } = req.body;
+
+        const isExists = await Post.findOne({ _id: id });
+
+        if (!isExists) 
+        {
+            return res.status(400).json({
+                success: false,
+                msg: "Post Not Found. Update aborted.",
+            });
+        }
+
+        var updateObj = {
+            title,
+            description
+        };
+
+        if (req.body.categories) {
+            updateObj.categories = req.body.categories;
+        }
+
+        const updatedPost = await Post.findByIdAndUpdate(id, {
+            $set: updateObj
+        }, { new: true });
+
+        return res.status(200).json({
+            success: true,
+            msg: 'Post Data Updated Successfully!',
+            data: updatedPost
+        });
+
+    } catch (error) {
         return res.status(400).json({
             success: false,
             msg: error.message,
         });
     }
-
 };
 
 
