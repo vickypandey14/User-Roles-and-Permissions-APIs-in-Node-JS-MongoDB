0906dfbb8605ffa2e77265d62d9100b7d0e79255
vickypandey14 <vickypandeyts@gmail.com>
2024-03-12 22:08:24 +0530

    delete user api complete, postman collection data also updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -193,8 +193,48 @@ const updateUser = async(req, res) => {
 // Delete User API Method
 
 const deleteUser = async(req, res) => {
-    
 
+    try {
+
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                success: false,
+                msg: 'Errors',
+                errors: errors.array()
+            });
+        }
+
+        const { id } = req.body;
+
+        const isExists = await User.findOne({
+            _id: id
+        });
+
+        if (!isExists) {
+            return res.status(400).json({
+                success: false,
+                msg: 'User Not Found.',
+            });
+        }
+
+        await User.findByIdAndDelete({
+            _id: id
+        });
+
+        return res.status(200).json({
+            success: true,
+            msg: 'User Record Deleted Succcessfully!',
+        });
+        
+    } catch (error) 
+    {
+        return res.status(400).json({
+            success: false,
+            msg: error.message,
+        });
+    }
     
 }
 
